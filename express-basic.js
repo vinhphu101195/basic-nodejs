cbf2f56b6bce4747675e9f6a9195326c1c537cd4
PhuChau <vinhphu101195@gmail.com>
2019-06-10 19:16:14 +0300

    post request

--- express-basic.js
+++ express-basic.js
@@ -1,8 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.set("view engine", "ejs");
+app.use("/assets", express.static("assets"));
 
 // app.get("/", function(req, res) {
 //   res.sendFile(__dirname + "/index.html");
@@ -16,9 +20,19 @@ app.get("/contact", function(req, res) {
   res.send("this is the contact page");
 });
 
+//query strings
+// /contact?dept=marketing&person=joe it will return {dept: marketing, person: joe}
+app.get("/contact", function(req, res) {
+  res.render("contact", { qs: req.query });
+});
 // app.get("/profile/:id", function(req, res) {
 //   res.send("you request to see profile with the id of " + req.params.id);
 // });
+
+app.post("/contact", urlencodedParser, function(req, res) {
+  console.log(req.body);
+  res.render("contact-success", { data: req.body });
+});
 
 app.get("/profile/:name", function(req, res) {
   var data = {
