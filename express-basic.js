aeaf4df34e13fa5a01165b23de0b9495e58e22ce
PhuChau <vinhphu101195@gmail.com>
2019-06-10 16:26:01 +0300

    template engines with ejs

--- express-basic.js
+++ express-basic.js
@@ -2,16 +2,31 @@ var express = require("express");
 
 var app = express();
 
+app.set("view engine", "ejs");
+
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
 app.get("/", function(req, res) {
-  res.send("this is the homepage");
+  res.render("index");
 });
 
 app.get("/contact", function(req, res) {
   res.send("this is the contact page");
 });
 
-app.get("/profile/:id", function(req, res) {
-  res.send("you request to see profile with the id of " + req.params.id);
+// app.get("/profile/:id", function(req, res) {
+//   res.send("you request to see profile with the id of " + req.params.id);
+// });
+
+app.get("/profile/:name", function(req, res) {
+  var data = {
+    age: 24,
+    job: "student",
+    hobbie: ["eating", "gym", "basketball"]
+  };
+  res.render("profile", { person: req.params.name, data: data });
 });
 
 app.listen(3000);
